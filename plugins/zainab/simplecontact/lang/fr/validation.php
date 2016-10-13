40bca7621bdcda459cb99cc294aec0171674c418
spectralshard <5seller>
2016-10-12 21:41:52 -0400

    Ajout du fichier de langue français.

---
+++ plugins/zainab/simplecontact/lang/fr/validation.php
@@ -0,0 +1,20 @@
+<?php return [
+        'custom' => [
+            'name' => [
+                'required' => 'Votre nom est requis pour savoir à qui je dois m\'adresser!',
+            ],
+            'email' => [
+                'required' => 'Votre adresse courriel est requise!',
+                'email' => 'SVP, ajoutez une adresse courriel valide!'
+            ],
+            'subject' => [
+                'required' => 'SVP, ajoutez un sujet à votre message!',
+            ],
+            'message' => [
+                'required' => 'Aucun contenu dans votre message. Avez-vous une question?',
+            ],
+            'reCAPTCHA' => [
+                'required' => 'Wow! Êtes-vous vraiment un être humain? Remplisez à nouveau le Captcha SVP.'
+            ]
+        ]
+    ];
